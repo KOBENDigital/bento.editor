bdbcc779a043eca72a4d4212c505a5cd719571cd
EwneoN <samuelbutler87@gmail.com>
2020-06-24 16:14:09 +1000

    Fixed issues with last release and added missing packages. Also improved build batch files to work with different MSBuild locations.

--- src/BuildPackage/Build-Core.bat
+++ src/BuildPackage/Build-Core.bat
@@ -1,2 +1,8 @@
-Call nuget.exe restore ..\Bento.sln
-Call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" Bento.Core/Package.build.xml /p:Configuration=Release
+set msbuildpath="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+
+if not exist %msbuildpath% (
+  set msbuildpath="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
+)
+
+call nuget.exe restore ..\Bento.sln
+call %msbuildpath% Bento.Core/Package.build.xml /p:Configuration=Release
