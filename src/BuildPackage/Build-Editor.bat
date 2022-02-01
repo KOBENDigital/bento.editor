fa5c2c960b9c794ffc7f51472376aaf7a7baf4b9
jake williamson <jake@koben.com.au>
2022-02-01 14:44:49 +1000

    removed the old build system

--- src/BuildPackage/Build-Editor.bat
+++
@@ -1,8 +0,0 @@
-set msbuildpath="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
-
-if not exist %msbuildpath% (
-  set msbuildpath="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
-)
-
-call nuget.exe restore ..\Bento.sln
-call %msbuildpath% Bento.Editor/Package.build.xml /p:Configuration=Release
